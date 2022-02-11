c9becffcc8145dc7eae182a544533744c9da9c6f
alenkakrp <alenka.krp@gmail.com>
2022-02-11 11:50:49 -0500

    Addit code changes up to and including task 9

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -39,10 +39,17 @@ Object.defineProperty(Product.prototype, "daysToExpire", {
   });
 // Add method getDetails to Product here
 
+Product.prototype.getDetails = function () {
+    return `Product Name: ${this.name} , Product Price: ${this.price}`;
+}
 // Define the MagicProduct class here
-
+function MagicProduct(id, name, price, expiryDate, points, isBonus) {
+    Product.call(this, id, name, price, expiryDate);
+    this.points= points;
+    this.isBonus = isBonus;
+}
 // Establish inheritance between Product() & MagicProduct() here
-
+MagicProduct.prototype = Object.create(Product.prototype);
 // Define Rating class here
 
 // Complete the loadProducts function
